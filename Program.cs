51736e49ed25938413a9096a9dddecf7862e117c
JuanPaz-Dev <jppaz2002@gmail.com>
2022-06-03 18:41:16 -0300

    SDK.NET-LINUX

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cual es tu nombre?");
+            var nombre = Console.ReadLine();
+            var fechaActual = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hola, {nombre}, la fecha actual es {fechaActual:d} y son las {fechaActual:t}!");
+            Console.Write($"{Environment.NewLine}Presione una tecla para finalizar...");
+            Console.ReadKey(true);
+        }
+    }
+}
